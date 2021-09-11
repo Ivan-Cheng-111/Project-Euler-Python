1bd8e994e05ca17c299d487dd608bdcf69afdab9
Ivan Cheng <57711268+ivancheng111@users.noreply.github.com>
2021-09-11 20:03:52 +0630

    Added new method, Removed a method, Added comments

--- 10 Summation of primes.py
+++ 10 Summation of primes.py
@@ -4,52 +4,67 @@ Finished: Sep/10/2021 9:45pm
 """
 
 from itertools import repeat
-from bisect import bisect_right
 from math import isqrt
 
-# classical sieve of eratosthenes
+# naive implementation
+def compute3(n):
+	# base cases, 1 and lower has no prime, 2 and lower prime sum is 2
+	if n < 2:
+		return 0
+	if n == 2:
+		return n
+    
+	sum = 2
+    # start from 3 skip by 2 (skip even numbers)
+	for i in range(3,n+1,2):
+        # sqrt(i) is the largest possible factor
+		for j in range(3,isqrt(i)+1,2):
+			if i % j == 0:
+				break
+		else:
+			sum += i
+	return sum
+
+# sieve of eratosthenes
 def compute2(n):
-    if n <= 2:
-        return []
+    # base case 1 and lower has no prime
+    if n < 2:
+        return 0
+    
+    # make boolen list sized n, initalized as True
     is_prime = [True] * n
     # mark out 0 and 1 as False
     is_prime[0] = is_prime[1] = False
 
+    # mark out all even numbers 4 and above
     if n % 2 == 0:
         for i in range(2*2, n, 2):
             is_prime[i] = False
-    # if True, mark out its multiples
-    for i in range(3, isqrt(n),2):
+    
+    # if True, mark out its multiples as False
+    for i in range(3, isqrt(n)+1,2):
         if is_prime[i]:
             for x in range(i*i, n, i):
                 is_prime[x] = False
     
+    # return sum of primes
     return sum(i for i in range(n) if is_prime[i])
 
-# segemented sieve of eratosthenes (faster)
+# loop optimized sieve of eratosthenes
 def compute(n):
-    primes = [2, 3, 5, 7]
-    end_segment = 1
-    extend_at_most_n_segments_target = 10
+    if n < 2:
+        return 0
+    is_prime = [True] * n
+    is_prime[0] = is_prime[1] = False
 
-    while primes[-1] < n:
-        k = end_segment
-        a = min(extend_at_most_n_segments_target, len(primes) - 1 - k)
-        p, q = primes[k], primes[k + a]
-        segment = range(p * p, q * q)
-        segment_min = min(segment)
-        segment_len = len(segment)
-        is_prime = [True] * segment_len
-
-        for i in range(k + a):
-            pk = primes[i]
-            start = segment_min + ((pk - (segment_min % pk)) % pk)
-            is_prime[start - segment_min::pk] = repeat(False, len(range(start - segment_min, segment_len, pk)))
-
-        primes.extend([x for x, is_prime in zip(segment, is_prime) if is_prime])
-        end_segment += a
+    if n % 2 == 0:
+        is_prime[2*2::2] = repeat(False, len(range(2*2, n, 2)))
     
-    return sum(primes[:bisect_right(primes,n)])
+    for i in range(3,isqrt(n)+1,2):
+        if is_prime[i]:
+            is_prime[i*i::i] = repeat(False, len(range(i*i, n, i)))
+    
+    return sum(i for i in range(n) if is_prime[i])
 
 n = 2_000_000
-print(f"Sum of All the Primes Below 2 Million Is: {compute(n)}")
+print(f"Sum of All the Primes Below {n:_} Is: {compute(n)}")
