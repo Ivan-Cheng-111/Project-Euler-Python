822d081af656521c4106541bad15e0f135589289
Ivan Cheng <57711268+ivancheng111@users.noreply.github.com>
2021-09-14 21:20:34 +0630

    Create 22 Names scores.py

---
+++ 22 Names scores.py
@@ -0,0 +1,18 @@
+""" Problem 22
+Start: Sep/14/2021 9:00pm
+Finished: Sep/14/2021 7:45pm
+"""
+
+txt = open("C:\\Users\\Ivan\\Documents\\.Programming\\Project Euler\\.resources\\22 names.txt", "r")
+names = [name.strip("\"") for name in txt.read().split(",")]
+txt.close()
+names.sort()
+
+# zip position of sort names and numerical score of names
+name_ranks = zip(range(1,len(names)+1),[sum(map(lambda x: ord(x)-64,name)) for name in names])
+name_scores = []
+# multiply position and score
+for rank, score in name_ranks:
+    name_scores.append(rank*score)
+
+print(f"Total Score of All Names Is: {sum(name_scores)}")
