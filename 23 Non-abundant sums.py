1cc866cb5015b54250731651bc0e7be4f27732f3
Ivan Cheng <57711268+ivancheng111@users.noreply.github.com>
2021-09-14 23:34:04 +0630

    Create 23 Non-abundant sums.py

---
+++ 23 Non-abundant sums.py
@@ -0,0 +1,38 @@
+""" Problem 23
+Start: Sep/14/2021 9:25pm
+Finished: Sep/14/2021 9:20pm
+"""
+
+from math import ceil
+
+# sieve of eratosthenes inspired
+def compute():
+    # largest number that cannot be represented by the sum of two abundant numbers
+    lower = 12
+    LIMIT = 28123
+
+    # make list of 0s sized LIMIT
+    divisor_sums = [0] * LIMIT
+    for n in range(1,ceil(LIMIT/2)):
+        for pos in range(n*2,LIMIT,n):
+            divisor_sums[pos] += n
+
+    abundant = [n for n,divisor_sum in enumerate(divisor_sums) if n < divisor_sum]
+    
+    
+    # make boolen list sized LIMIT
+    is_not_abundant = [True] * LIMIT
+
+    # mark out all the numbers with sum of abundant numbers
+    for n1 in abundant:
+        for n2 in abundant:
+            sum_of_abundant = n1 + n2
+            if sum_of_abundant and sum_of_abundant < LIMIT:
+                is_not_abundant[sum_of_abundant] = False
+            else:
+                break
+
+    # sum marked true (aka the non-abundant numbers)
+    return sum(i for i in range(LIMIT) if is_not_abundant[i])
+
+print(f"The Sum of All Numbers Not The Sum of Two Abundant Numbers Is: {compute()}")
