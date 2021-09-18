b47f52c7a70b20191171956577f612e5093cf1a3
Ivan Cheng <57711268+ivancheng111@users.noreply.github.com>
2021-09-18 20:14:54 +0630

    Create 28 Number spiral diagonals.py

---
+++ 28 Number spiral diagonals.py
@@ -0,0 +1,15 @@
+""" Problem 28
+Start: Sep/17/2021 11:35pm
+Finished: Sep/18/2021 12:00am
+"""
+
+def compute(MAX):
+	sum_of_diags = 1
+	for n in range(3,MAX+1,2):
+		# top-right n^2, top-left n^2-n+1, bot-left n^-2n+2, bot-right n^3n+3, sum all
+		# 4n^2-6n+6
+		sum_of_diags += 4*n*n - 6*n + 6
+	return sum_of_diags
+
+n = 1001
+print(compute(n))
